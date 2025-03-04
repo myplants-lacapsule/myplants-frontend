76334c73c76ebb5094f9d07bf5fa4caa9eb99fd0
Emile Karacostas <emile.karacostas@proton.me>
2025-03-04 13:18:17 +0100

    updated HomeScreen

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,18 +1,36 @@
 import { StyleSheet, Text, View } from "react-native";
+import { useSelector } from "react-redux";
 import FontAwesome5 from "react-native-vector-icons/FontAwesome5";
+import CustomButton from "../components/CustomButton";
 
 export default function HomeScreen() {
+	const userInStore = useSelector((state) => state.user.value);
+
   return (
     <View style={styles.container}>
       <View style={styles.topContainer}>
-        <Text style={styles.hello}>Bonjour,</Text>
-				<Text style={styles.hello}>Pauline !</Text>d
+        <Text style={styles.hello}>Bonjour, {userInStore.username} !</Text>
         <View style={styles.iconContainer}>
-          <FontAwesome5 style={styles.icon} name="bell" solid={true} />
-          <FontAwesome5 style={styles.icon} name="user-circle" solid={true} />
+          <FontAwesome5
+            style={styles.icon}
+            name="bell"
+            size={25}
+            solid={true}
+          />
+          <FontAwesome5
+            style={styles.icon}
+            name="user-circle"
+            size={40}
+            solid={true}
+          />
         </View>
       </View>
       <Text style={styles.title}>Mes plantes</Text>
+      <CustomButton
+        style={styles.text}
+        iconName="plus-circle"
+        text="Ajouter une plante"
+      />
     </View>
   );
 }
@@ -21,7 +39,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#F1F0E9",
-    alignItems: "center",
   },
   topContainer: {
     flexDirection: "row",
@@ -29,25 +46,33 @@ const styles = StyleSheet.create({
     alignItems: "center",
     width: "100%",
     padding: 20,
-		backgroundColor: "pink",
-		marginTop: '10%',
+    marginTop: "10%",
   },
   hello: {
     color: "#2D5334",
-		fontSize: 30,
-		fontFamily: "Merriweather",
+    fontSize: 24,
+    fontFamily: "Merriweather",
   },
   iconContainer: {
     display: "flex",
     flexDirection: "row",
-		backgroundColor: "yellow",
+    justifyContent: "space-between",
+    alignItems: "center",
+    width: 80,
+    height: 45,
   },
   icon: {
-    size: 100,
     color: "#2D5334",
   },
-	title: {
-		color: "#2D5334",
-		fontFamily: "Merriweather",
-	},
+  title: {
+    color: "#2D5334",
+    fontSize: 30,
+    marginTop: 30,
+    paddingLeft: 15,
+    fontFamily: "Merriweather-Bold",
+  },
+  text: {
+    fontSize: 25,
+    fontFamily: "OpenSans-Regular",
+  },
 });
