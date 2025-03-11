039a5cc058594d18f1bec8b7d79cf0306c16120c
Emile Karacostas <emile.karacostas@proton.me>
2025-03-11 13:28:10 +0100

    style

--- components/Notifications.js
+++ components/Notifications.js
@@ -1,43 +1,38 @@
 import React from "react";
-
-import { View, Text, StyleSheet } from "react-native";
-
+import { StyleSheet, Text, View } from "react-native";
 import moment from "moment";
 
 export default function Notifications(props) {
-  // utilisation de moment pour afficher la date du dernier arrosage
+  // Utilisation de moment pour afficher la date du dernier arrosage
   const date = moment(props.lastWatering);
   const lastWatered = moment(date).format("MMMM Do YYYY");
 
   return (
-    <View style={styles.notifications}>
+    <View style={styles.container}>
       <Text style={styles.text}>
-        Your plant <Text style={styles.textbold}>{props.name}</Text> needs water !
+        Your plant <Text style={styles.boldText}>{props.name}</Text> needs water !
       </Text>
       <Text style={styles.text}>
-        The last watering was : <Text style={styles.textbold}>{lastWatered}</Text>
+        The last watering was : <Text style={styles.boldText}>{lastWatered}</Text>
       </Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  notifications: {
-    justifyContent: "center",
-    backgroundColor: "white",
-    width: "90%",
+  container: {
+    width: "100%",
     height: 80,
-    padding: 10,
-    borderBottomWidth: 2,
-    borderColor: "#F8F3D9",
-    borderRadius: 8,
-    // alignItems: "center",
+    padding: 13,
+		margin: 3,
+    justifyContent: "center",
+		backgroundColor: 'white',
   },
   text: {
     fontFamily: "OpenSans-Regular",
-    fontSize: 15,
+		fontSize: 16,
   },
-  textbold: {
+  boldText: {
     fontFamily: "OpenSans-Bold",
     color: "#2D5334",
     fontSize: 16,
