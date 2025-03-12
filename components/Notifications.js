54f5ded24e0ad5cf6aa3f6ab5c533de5e7a5c4dc
matthieup-git <matthieu.posnic@gmail.com>
2025-03-12 17:08:07 +0100

    push

--- components/Notifications.js
+++ components/Notifications.js
@@ -4,23 +4,30 @@ import moment from "moment";
 
 import { useNavigation } from "@react-navigation/native";
 
+import FontAwesome from "react-native-vector-icons/FontAwesome";
+
+
 export default function Notifications(props) {
+
   // Utilisation de moment pour afficher la date du dernier arrosage
-  console.log("props", props)
   const date = moment(props.lastWatering);
   const lastWatered = moment(date).format("MMMM Do YYYY");
 
-    const navigation = useNavigation();
-
+  const navigation = useNavigation();
 
   return (
     <TouchableOpacity style={styles.container} onPress={() => navigation.navigate("FullDetailsPlant", { plantDetails: props })} >
-      <Text style={styles.text}>
-        Your plant <Text style={styles.boldText}>{props.name}</Text> needs water !
-      </Text>
-      <Text style={styles.text}>
-        The last watering was on: <Text style={styles.boldText}>{lastWatered}</Text>
-      </Text>
+      <View style={styles.contentContainer}>
+        <FontAwesome name="exclamation-triangle" size={30} color="#BC4749" />
+        <View style={styles.textContainer}>
+          <Text style={styles.text}>
+            Your plant <Text style={styles.boldText}>{props.name}</Text> needs water !
+          </Text>
+          <Text style={styles.text}>
+            The last watering was on: <Text style={styles.boldText}>{lastWatered}</Text>
+          </Text>
+        </View>
+      </View>
     </TouchableOpacity>
   );
 }
@@ -30,17 +37,23 @@ const styles = StyleSheet.create({
     width: "100%",
     height: 80,
     padding: 13,
-		margin: 3,
+    margin: 3,
     justifyContent: "center",
-		backgroundColor: 'white',
+    backgroundColor: 'white',
   },
   text: {
     fontFamily: "OpenSans-Regular",
-		fontSize: 14,
+    fontSize: 14,
   },
   boldText: {
     fontFamily: "OpenSans-Bold",
     color: "#2D5334",
     fontSize: 16,
   },
+  contentContainer: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    gap: 10,
+  }
 });
