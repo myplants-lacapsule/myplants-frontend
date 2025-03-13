a1d1fdc34bfda8b76ed453cf4d4591312876cfcb
Emile Karacostas <emile.karacostas@proton.me>
2025-03-13 11:29:31 +0100

    sign in et up en try catch

--- screens/SignInScreen.js
+++ screens/SignInScreen.js
@@ -16,34 +16,36 @@ export default function SignInScreen() {
   const [signInPassword, setSignInPassword] = useState("");
   const [error, setError] = useState("");
 
-	// Fonction pour gérer la connexion de l'utilisateur
-  const handleConnection = () => {
-    if (!signInEmail) {
-      setError("Incorrect email address");
-      return;
-    }
-    if (!signInPassword) {
-      setError("Incorrect password");
-      return;
-    }
-
-    fetch(`${process.env.EXPO_PUBLIC_API_URL}/users/signin`, {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ email: signInEmail, password: signInPassword }),
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        if (data.result) {
-          dispatch(login({ username: data.username, token: data.token }));
-          setSignInEmail("");
-          setSignInPassword("");
-          setError("");
-          navigation.navigate("TabNavigator");
-        } else {
-          setError("This user does not exist");
-        }
+  // Fonction pour gérer la connexion de l'utilisateur
+  const handleConnection = async () => {
+    try {
+      if (!signInEmail) {
+        setError("Incorrect email address");
+        return;
+      }
+      if (!signInPassword) {
+        setError("Incorrect password");
+        return;
+      }
+      const response = await fetch(`${process.env.EXPO_PUBLIC_API_URL}/users/signin`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ email: signInEmail, password: signInPassword }),
       });
+      const data = await response.json();
+
+      if (data.result) {
+        dispatch(login({ username: data.username, token: data.token }));
+        setSignInEmail("");
+        setSignInPassword("");
+        setError("");
+        navigation.navigate("TabNavigator");
+      } else {
+        setError("This user does not exist");
+      }
+    } catch (error) {
+      console.error("Error signing in", error);
+    }
   };
 
   return (
@@ -62,13 +64,13 @@ export default function SignInScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-		paddingTop: 40,
+    paddingTop: 40,
     backgroundColor: "#F1F0E9",
   },
   registerContainer: {
     width: "80%",
-		alignSelf: 'center',
-		marginTop: 140,
+    alignSelf: "center",
+    marginTop: 140,
   },
   errorText: {
     width: "80%",
